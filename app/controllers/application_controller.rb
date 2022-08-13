fde29b676304e0aade85d2034b2b6f31acddfe3e
fernandaquetcia <fernandaquetcia@gmail.com>
2022-08-13 14:35:35 -0600

    fixing pundit x1000

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
   include Pundit
 
   # Pundit: white-list approach.
@@ -12,6 +14,14 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, [:photo]])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, [:photo]])
+  end
 
   private
 
