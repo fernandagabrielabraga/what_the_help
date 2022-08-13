f71eb9c7c6c42517db2cd418cc05ebc8e0030d4a
Angela Amaral <angela.hawerroth@gmail.com>
2022-08-13 14:24:20 -0300

    Configurated some policy donation

--- app/policies/donation_policy.rb
+++ app/policies/donation_policy.rb
@@ -1,8 +1,26 @@
 class DonationPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.where(is_donation: true)
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+
+  def destroy?
+    record.user == user
   end
 end
