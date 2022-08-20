7872222f73788bddf7049f3a4d5176f93dea3cb4
fernanda braga <fernanda.gabriela.braga@gmail.com>
2022-08-20 08:25:19 -0600

    profile added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-
   def avatar_params
     params.require(:user).permit(:title, :body, :avatar)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    authorize @user
   end
 end
