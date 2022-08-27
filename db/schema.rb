091b4971ae85884cc1ae7822beb613e947dc70ef
Angela Amaral <angela.hawerroth@gmail.com>
2022-08-27 15:50:41 -0300

    Created the Review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_205036) do
+ActiveRecord::Schema.define(version: 2022_08_27_184858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,8 +70,8 @@ ActiveRecord::Schema.define(version: 2022_08_23_205036) do
 
   create_table "donations", force: :cascade do |t|
     t.string "donation_name"
-    t.string "description"
-    t.text "location"
+    t.text "description"
+    t.string "location"
     t.integer "quantity"
     t.bigint "user_id", null: false
     t.bigint "category_id", null: false
@@ -91,6 +91,15 @@ ActiveRecord::Schema.define(version: 2022_08_23_205036) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -122,4 +131,5 @@ ActiveRecord::Schema.define(version: 2022_08_23_205036) do
   add_foreign_key "donations", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "reviews", "users"
 end
